96f007c80fd8572fb868d75d2740bd4551da57b3
Kintelligence <kennyquach1@gmail.com>
2024-12-07 12:41:48 +0100

    feat: add day-07.2

--- day-07/src/lib.rs
+++ day-07/src/lib.rs
@@ -76,8 +76,62 @@ mod part_1_tests {
     }
 }
 
+fn check_2(calibration: &Calibration) -> bool {
+    check_recursive_2(calibration, calibration.inputs[0], 1)
+}
+
+fn check_recursive_2(calibration: &Calibration, sum: u64, i: usize) -> bool {
+    if i == calibration.inputs.len() {
+        return sum == calibration.result;
+    }
+
+    if sum > calibration.result {
+        return false;
+    }
+
+    if check_recursive_2(calibration, sum + calibration.inputs[i], i + 1) {
+        return true;
+    }
+
+    if check_recursive_2(calibration, sum * calibration.inputs[i], i + 1) {
+        return true;
+    }
+
+    if let Some(concat_sum) = concatenate(sum, calibration.inputs[i]) {
+        if check_recursive_2(calibration, concat_sum, i + 1) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn concatenate(mut left: u64, mut right: u64) -> Option<u64> {
+    if right == 0 {
+        return left.checked_mul(10);
+    }
+
+    let result = right;
+
+    while right > 0 {
+        right /= 10;
+        if let Some(l) = left.checked_mul(10) {
+            left = l;
+        } else {
+            return None;
+        }
+    }
+
+    return Some(left + result);
+}
+
 pub fn part_2(_input: &str) -> Solution {
-    Solution::None
+    let calibrations = parse(_input);
+    calibrations
+        .iter()
+        .filter_map(|c| if check_2(&c) { Some(c.result) } else { None })
+        .sum::<u64>()
+        .into()
 }
 
 #[cfg(test)]
@@ -85,13 +139,23 @@ mod part_2_tests {
     use crate::*;
     use test_case::test_case;
 
-    #[test_case(include_str!("_test.txt"), 47)]
-    fn example_input(input: &str, expected: usize) {
+    #[test_case(include_str!("_test.txt"), 11387)]
+    fn example_input(input: &str, expected: u64) {
+        assert_eq!(part_2(input), expected.into());
+    }
+
+    #[test_case(include_str!("_edge.txt"), 0)]
+    fn edge_input(input: &str, expected: u64) {
         assert_eq!(part_2(input), expected.into());
     }
 
     #[test_case(0)]
-    fn real_input(expected: usize) {
+    fn real_input(expected: u64) {
         assert_eq!(part_2(_INPUT), expected.into());
+    }
+
+    #[test_case(123, 321, 123321)]
+    fn concat_test(left: u64, right: u64, expected: u64) {
+        assert_eq!(concatenate(left, right), Some(expected));
     }
 }
